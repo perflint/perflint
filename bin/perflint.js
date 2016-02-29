b3c9010b951e7f7d2bf3eb5f629010b027124670
Matthew Harrison-Jones <matthojo@gmail.com>
2016-02-29 10:56:21 +0000

    Add support for WebPageTest

--- bin/perflint.js
+++ bin/perflint.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-require('../lib/cli').interpret(process.argv)
+var debug = (process.argv.indexOf('--debug') > -1)
+
+// must do this initialization *before* other requires in order to work
+if (debug) {
+  require('debug').enable('perflint:*')
+}
+
+var cli = require('../lib/cli')
+cli.interpret(process.argv)
