26374e9ada4efd7b90e8f8a937bc9a05046f6fcb
Matthew Harrison-Jones <matthojo@gmail.com>
2016-02-29 16:46:52 +0000

    Add placeholder WPT tests

---
+++ tests/lib/service/webpagetest.js
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview Tests for WebPageTest module
+ * @author Matthew Harrison-Jones
+ */
+'use-strict'
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+// var assert = require('chai').assert,
+//     wpt = require('../../../lib/service/webpagetest')
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+describe('WebPageTest', function() {
+  describe('getResults()', function() {
+
+    it('should return results for new tests')
+
+    it('should return results for existing tests')
+
+  })
+
+  describe('translateResults()', function() {
+
+    it('should return error with invalid \'average\' in config')
+
+    it('should return error with invalid \'view\' in config')
+
+    it('should return formatted results')
+
+  })
+})
