46e433b3bfe63f26baa5d5470ca1f2b79e59ec39
Matthew Harrison-Jones <matthojo@gmail.com>
2016-03-30 12:54:16 +0100

    New: Add tests for WPT API

--- tests/lib/service/webpagetest.js
+++ tests/lib/service/webpagetest.js
@@ -7,20 +7,53 @@
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
-// var assert = require('chai').assert,
-//     wpt = require('../../../lib/service/webpagetest')
+var assert        = require('chai').assert,
+    wpt           = require('../../../lib/service/webpagetest'),
+    defaultConfig = require('../fixtures/config/wpt/.perflint.json')
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 describe('WebPageTest', function() {
+
   describe('getResults()', function() {
+    var config = defaultConfig
+    config.key = process.env.PERFLINT_KEY
 
-    it('should return an error when an invalid server is defined')
+    beforeEach(function() {
+      // Reset config
+      config.server = 'www.webpagetest.org'
+      config.URL = 'http://example.com'
+      delete config.test
+    })
 
-    it('should return results for new tests')
+    it('should return results for new tests', function(done) {
+      this.timeout(config.timeout * 2000)
+      wpt.getResults(config, function(err, data) {
+        assert.equal(err, null)
+        assert.isObject(data, 'Data is an object')
+        done()
+      })
+    })
 
-    it('should return results for existing tests')
+    it('should return results for existing tests', function(done) {
+      delete config.URL
+      config.test = '160317_RP_N76'
+      wpt.getResults(config, function(err, data) {
+        assert.equal(err, null)
+        assert.isObject(data, 'Data is an object')
+        done()
+      })
+    })
+
+    it('should return an error when an invalid server is defined', function(done) {
+      config.server = 'www.invalidurl.com'
+      wpt.getResults(config, function(err, data) {
+        assert.equal(err, 1)
+        assert.equal(data, null)
+        done()
+      })
+    })
 
   })
 
